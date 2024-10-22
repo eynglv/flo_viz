4e6ee3775ce94310ce58de5d13bc2b473c326a24
ElvyYang <eynglv@gmail.com>
2024-10-22 11:39:41 -0700

    initialize project - load data with csv - start webscraper

--- olmsted_star/olmsted_star/dataset.py
+++ olmsted_star/olmsted_star/dataset.py
@@ -1,1 +1,21 @@
+import json
+import pandas as pd
+import csv
+import math
+
 # Scripts to download or generate data
+filename = "/Users/elvyyang/Documents/flo_viz/olmsted_star/data/external/cartodb-query.csv"
+
+def main():
+    pd.options.display.max_rows = 10
+    carto_data = pd.read_csv(filename)
+    data = pd.DataFrame(carto_data)
+    print(data)
+    data = data.drop(columns=['cartodb_id', 'frla_plans', 'icon', 'archival_records', 'images'])   
+    # filter by nan date_range
+    project_types = pd.Series(data['project_type'])
+    
+    
+
+if __name__ == "__main__":
+    main()
