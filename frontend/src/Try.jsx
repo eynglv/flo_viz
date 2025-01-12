5685ffe4a980b2f2f69880d86c6894779f821adc
ElvyYang <eynglv@gmail.com>
2025-01-12 01:41:23 -0800

    sweet suffering jzus .. got the animation to sync

--- frontend/src/Try.jsx
+++ frontend/src/Try.jsx
@@ -1,6 +1,6 @@
 import "@asymmetrik/leaflet-d3";
 import { MapContainer, TileLayer, GeoJSON, useMap } from "react-leaflet";
-import { useState, useEffect, useRef, useLayoutEffect } from "react";
+import { useState, useEffect, useRef, useCallback, useMemo } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
@@ -8,7 +8,8 @@ import { useGSAP } from "@gsap/react";
 import { coords as stateCoords } from "./helpers/constants";
 import { DotDensity, DotDistribution } from "./plots";
 import Legend from "./components/Legend";
-import Modal from "./components/Modal";
+import { newYorkText } from "./helpers/text";
+import { sort } from "d3";
 
 // import "./basemap.css";
 
@@ -27,45 +28,65 @@ const BaseMap = ({
   const mapRef = useRef(null);
   const mapContainerRef = useRef();
   const containerRef = useRef();
+  const textRef = useRef();
 
-  const [openModal, setOpenModal] = useState(false);
-  const [modalPark, setModalPark] = useState(null);
+  const [mapHeight, setMapHeight] = useState(0);
 
   let offset = 3;
-  const reorgParksData = parksData.features.map((feature, index) => {
-    const { properties } = feature;
-    const { name: parkName } = properties;
-    if (parkName === "Central Park") {
-      feature["appearanceOrder"] = 0;
-      offset -= 1;
-    } else if (parkName === "Prospect Park") {
-      feature["appearanceOrder"] = 1;
-      offset -= 1;
-    } else if (parkName === "Sunset Park") {
-      feature["appearanceOrder"] = 2;
-      offset -= 1;
-    } else {
-      feature["appearanceOrder"] = index + offset;
-    }
-    return feature;
-  });
+  const reorgParksData = useMemo(
+    () =>
+      parksData.features.map((feature, index) => {
+        const { properties } = feature;
+        const { name: parkName } = properties;
+        if (parkName === "Central Park") {
+          feature["appearanceOrder"] = 0;
+          offset -= 1;
+        } else if (parkName === "Prospect Park") {
+          feature["appearanceOrder"] = 1;
+          offset -= 1;
+        } else if (parkName === "Sunset Park") {
+          feature["appearanceOrder"] = 2;
+          offset -= 1;
+        } else {
+          feature["appearanceOrder"] = index + offset;
+        }
+        return feature;
+      }),
+    []
+  );
 
-  const sorted = Object.values(reorgParksData).sort((parkData1, parkData2) => {
-    const { appearanceOrder: firstAppearance } = parkData1;
-    const { appearanceOrder: secondAppearance } = parkData2;
-    return firstAppearance - secondAppearance;
-  });
+  const sorted = useMemo(
+    () =>
+      Object.values(reorgParksData).sort((parkData1, parkData2) => {
+        const { appearanceOrder: firstAppearance } = parkData1;
+        const { appearanceOrder: secondAppearance } = parkData2;
+        return firstAppearance - secondAppearance;
+      }),
+    [reorgParksData]
+  );
+  //   console.log(sorted);
 
-  const coords = sorted.map((feature, index) => {
-    if (index === 0) return [40.78166117469302, -73.96880877926723];
-    const coords = feature.geometry.coordinates[0][0][0];
-    return [coords[1], coords[0]];
-  });
+  //   console.log("LENGTH", sorted.length);
 
-  useEffect(() => {
-    const handleScroll = (self) => {
+  const coords = useMemo(
+    () =>
+      sorted.map((feature, index) => {
+        if (index === 0) return [40.78166117469302, -73.96880877926723];
+        const coords = feature.geometry.coordinates[0][0][0];
+        return [coords[1], coords[0]];
+      }),
+    [sorted]
+  );
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleScroll = useCallback(
+    (self) => {
       if (self.progress === 1) return;
       let newIndex = Math.floor(self.progress * coords.length);
+      console.log("progress", self.progress.toPrecision(3));
+      console.log("index", newIndex);
+      setCurrentIndex(newIndex);
 
       if (mapRef.current) {
         const newCoords = coords[newIndex];
@@ -75,15 +96,15 @@ const BaseMap = ({
           duration: 1,
         });
       }
-    };
+    },
+    [coords]
+  );
 
+  useEffect(() => {
     const ctx = gsap.context(() => {
       gsap.to(mapContainerRef.current, {
         scrollTrigger: {
           trigger: mapContainerRef.current,
-          start: "50% 50%",
-          end: "+=3000 top",
-          toggleActions: "play pause resume reset",
           scrub: true,
           pin: "#pinElement",
           pinSpacing: false,
@@ -94,52 +115,49 @@ const BaseMap = ({
     }, containerRef);
 
     return () => ctx.revert();
-  }, [coords]);
+  }, [coords, handleScroll, mapHeight]);
 
-  //   useGSAP(
-  //     () => {
-  //       if (mapContainerRef.current) {
-  //         ScrollTrigger.create({
-  //           trigger: mapContainerRef.current,
-  //           start: "50% 50%",
-  //           end: "+=100 top",
-  //           toggleActions: "play pause resume reset",
-  //           markers: true,
-  //           scrub: true,
-  //           pin: "#pinElement",
-  //           pinSpacing: false,
-  //           onUpdate: (self) => handleScroll(self),
-  //         });
-  //       }
-  //     },
-  //   {
-  //     scope: containerRef,
-  //     dependencies: [mapContainerRef.current],
-  //   }
-  //   );
+  useEffect(() => {
+    const ztx = gsap.context(() => {
+      gsap.to("#textContent", {
+        y: -window.innerHeight,
+        ease: "none",
+        scrollTrigger: {
+          trigger: mapContainerRef.current,
+          start: "50% 50%",
+          markers: true,
+          scrub: true,
+          id: "sentence",
+        },
+      });
+    }, containerRef);
 
-  useGSAP(() => {
-    gsap.to("#firstSentence", {
-      y: -window.innerHeight,
-      duration: 3,
-      ease: "power2.out",
-      scrollTrigger: {
-        trigger: mapContainerRef.current,
-        start: "50% 50%",
-        // half of map end length
-        end: "+=1500 top",
-        toggleActions: "play pause resume reset",
-        markers: true,
-        scrub: true,
-        id: "sentence",
-      },
-    });
-  });
+    return () => ztx.revert();
+  }, [mapHeight]);
+
+  useEffect(() => {
+    const updateRightColumnHeight = () => {
+      if (mapContainerRef.current) {
+        const { height } = mapContainerRef.current.getBoundingClientRect();
+        setMapHeight(height);
+      }
+    };
+
+    updateRightColumnHeight();
+    window.addEventListener("resize", updateRightColumnHeight);
+
+    return () => {
+      window.removeEventListener("resize", updateRightColumnHeight);
+    };
+  }, []);
 
   return (
     <div className='w-full h-screen flex justify-between' ref={containerRef}>
       <div className='h-screen w-full flex' id='pinElement'>
-        <div className='w-2/3 h-screen flex' ref={mapContainerRef}>
+        <div className='absolute top-0 left-0 p-4 z-[2000]' id='legend'>
+          {legend && <Legend />}
+        </div>
+        <div className='w-1/2 h-screen flex' ref={mapContainerRef}>
           <MapContainer
             center={stateCoords[state]}
             zoom={12}
@@ -172,73 +190,56 @@ const BaseMap = ({
                     direction: "center",
                     permanent: true,
                   });
-
-                  layer.on("click", () => {
-                    setOpenModal(true);
-                    setModalPark(feature.properties.name);
-                  });
                 }
               }}
             />
             {children}
             <DotDensity data={censusData} />
           </MapContainer>
-          <div className='mr-3' />
-          {legend && <Legend />}
         </div>
-        <div className='w-1/3 self-end translate-y-full' id='firstSentence'>
-          <div className='mb-[400px]' id='hello'>
-            <p className='text-2xl px-4'>
-              Around Central Park, Olmsted's first project, white households
-              represent a majority of the micro-neighborhoods in which the park
-              is a 10 minute walk away.
-            </p>
-            <DotDistribution
-              data={distributionData}
-              state='NYC'
-              selectedPark={"Central Park"}
-              layers={["race"]}
-              legend={false}
-            />
-          </div>
-          <div className='mb-[400px]' id='bye'>
-            <p className='text-2xl px-4'>
-              Prospect Park, in comparison, sees a more even ratio of white to
-              people of color.
-            </p>
-            <DotDistribution
-              data={distributionData}
-              state='NYC'
-              selectedPark={"Prospect Park"}
-              layers={["race"]}
-              legend={false}
-            />
-          </div>
-          {/* <div>
-            <p className='text-2xl px-4'>
-              Nearby, Sunset Park, is one of 4 parks where more people of color
-              reside.
-            </p>
-            <DotDistribution
-              data={distributionData}
-              state='NYC'
-              selectedPark={"Sunset Park"}
-              layers={["race"]}
-              legend={false}
-            />
-          </div> */}
+        <div
+          className='w-1/2 self-end translate-y-full'
+          id='textContent'
+          ref={textRef}
+        >
+          {Object.values(newYorkText).map(({ text, showTitle }, index) => {
+            const parkName = sorted[index].properties.name;
+            return (
+              <div
+                className={`h-[${mapHeight}px] ml-6`}
+                key={`${parkName}-text`}
+                id='text-block'
+              >
+                {text}
+                <DotDistribution
+                  data={distributionData}
+                  state='NYC'
+                  selectedPark={parkName}
+                  layers={["race"]}
+                  legend={false}
+                  showTitle={showTitle}
+                />
+              </div>
+            );
+          })}
+          {Object.values(sorted)
+            .slice(3)
+            .map(({ properties }) => {
+              const { name } = properties;
+              return (
+                <div className={`h-[${mapHeight}px] ml-6`} key={`${name}-text`}>
+                  <DotDistribution
+                    data={distributionData}
+                    state='NYC'
+                    selectedPark={name}
+                    layers={["race"]}
+                    legend={false}
+                    showTitle
+                  />
+                </div>
+              );
+            })}
         </div>
-        {openModal && (
-          <Modal isOpen={openModal} onClose={() => setOpenModal(false)}>
-            <DotDistribution
-              data={distributionData}
-              state='NYC'
-              selectedPark={modalPark}
-              layers={["race"]}
-              legend={false}
-            />
-          </Modal>
-        )}
       </div>
     </div>
   );
